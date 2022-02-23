41e56db448ce690241ad8d969b4d136b5ce42315
Sohrab sardari <sohrabsardari@outlook.com>
2022-02-24 00:31:38 +0330

    add builder project

---
+++ GOF/Creational Patterns/‌Builder/LaptopConfigurator/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public interface ILaptopBuilder
+{
+    void StartUpOperations();
+    void WithI7Cpu();
+    void WithI5Cpu();
+    void With16GigaByteRam();
+    void With32GigaByteRam();
+    void With4KScreen();
+    void WithFullHdScreen();
+    Laptop GetLaptop();
+}
+
+public class Laptop
+{
+}
